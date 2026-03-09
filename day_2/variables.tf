3e528566306ae5cb430c854a2cc53a4806e3fe44
Anil Jadhav <aniljadhav@gmail.com>
2026-03-09 14:52:28 +0530

    terraform day 2 practice

---
+++ day_2/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "passing value to ami id"
+  default = ""
+  type = string
+}
+
+variable "instance_type" {
+  description = "passing values to instance_type"
+  default = ""
+  type = string
+}
