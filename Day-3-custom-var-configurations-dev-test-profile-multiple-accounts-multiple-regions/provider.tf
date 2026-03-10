724c7403e7f3146e8114d4163fe33526d97e3bc3
Anil Jadhav <aniljadhav@gmail.com>
2026-03-10 14:46:16 +0530

    Custom variable terraform ractice

---
+++ Day-3-custom-var-configurations-dev-test-profile-multiple-accounts-multiple-regions/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+    alias = "devenv"
+    profile = "dev"
+#defualt region is us-east-1 but we can specify the region here if we want to use a different region
+#here dev = defualt profile in provider.tf
+}
+
+## test environment provider block##
+provider "aws" {
+    region = "us-east-1"
+    alias = "testenv" #if multiple providers are used then we have to give alias to the provider and then we can call that provider in the resource block
+    profile = "test"
+}
---
+++ Day-3-custom-var-configurations-dev-test-profile-multiple-accounts-multiple-regions/readme.md
@@ -0,0 +1,6 @@
+aws configure --profile dev
+aws configure --profile test
+
+#defualt region is us-east-1 but we can specify the region here if we want to use a different region
+
+#here dev = defualt profile in provider.tf
